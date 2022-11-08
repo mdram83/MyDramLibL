d78b7e4aa20f3e7d8afac24fce03f080f78d7c36
mdram83 <103069429+mdram83@users.noreply.github.com>
2022-11-08 21:28:15 +0100

    Added view for Music Albums

--- app/Http/Controllers/MusicAlbumController.php
+++ app/Http/Controllers/MusicAlbumController.php
@@ -8,6 +8,8 @@ class MusicAlbumController extends Controller
 {
     public function index()
     {
-
+        return view('music.index', [
+            'musicAlbums' => auth()->user()->musicAlbums()->latest()->paginate(10),
+        ]);
     }
 }
--- resources/views/components/music/list.blade.php
+++ resources/views/components/music/list.blade.php
@@ -1,6 +1,6 @@
-@props(['books'])
+@props(['musicAlbums'])
 
-@foreach ($books as $book)
+@foreach ($musicAlbums as $musicAlbum)
 
 <div class="first:pt-8 last:pb-8 py-2">
     <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
@@ -10,53 +10,53 @@
                 <!-- Thumbnail -->
                 <div class="pr-2 sm:basis-24 basis-16 flex-none">
                     <img class="max-h-24" alt="Thumbnail"
-                        src="{{ $book->item->thumbnail ?? '/images/thumbnails/book.png' }}"
+                        src="{{ $musicAlbum->item->thumbnail ?? '/images/thumbnails\music.png' }}"
                     >
                 </div>
 
                 <!-- Main data -->
                 <div class="flex-auto">
                     <p class="font-semibold text-lg leading-tight">
-                        <a href="/books/{{ $book->id }}" class="hover:underline">{{ $book->item->title }}</a>
+                        <a href="/books/{{ $musicAlbum->id }}" class="hover:underline">{{ $musicAlbum->item->title }}</a>
                     </p>
-                    @if ($book->item->artists)
+                    @if ($musicAlbum->item->artists)
                         <p class="text-sm pt-1 italic">
-                            @foreach ($book->item->artists as $artist){{($loop->index > 0 ? ', ' : '') . $artist->getName()}}@endforeach
+                            @foreach ($musicAlbum->item->artists as $artist){{($loop->index > 0 ? ', ' : '') . $artist->getName()}}@endforeach
                         </p>
                     @endif
 
-                    @if ($book->series)
-
+                    @if ($musicAlbum->volumes)
                         <p class="hidden sm:block text-sm pt-1 leading-tight">
-                            {{ $book->series }}
-
-                            @if ($book->volume)
-                                <br> Volume {{ $book->volume }}
-                            @endif
-
+                            Volumes: {{ $musicAlbum->volumes }}
                         </p>
-
                     @endif
+
+                    @if ($musicAlbum->duration)
+                        <p class="hidden sm:block text-sm pt-1 leading-tight">
+                            Duration: {{ $musicAlbum->duration }}
+                        </p>
+                    @endif
+
                 </div>
 
                 <!-- Details -->
                 <div class="hidden sm:block pl-2 sm:basis-64 flex-none text-right">
 
-                    @if ($book->item->published_at)
+                    @if ($musicAlbum->item->published_at)
                         <p class="text-xs">
-                            Published: {{ $book->item->published_at }}
+                            Published: {{ $musicAlbum->item->published_at }}
                         </p>
                     @endif
 
-                    @if ($book->item->publisher)
+                    @if ($musicAlbum->item->publisher)
                         <p class="text-xs pb-1">
-                            {{ $book->item->publisher->name }}
+                            {{ $musicAlbum->item->publisher->name }}
                         </p>
                     @endif
 
-                    @if ($book->item->tags)
+                    @if ($musicAlbum->item->tags)
                         <p class="text-xs leading-6">
-                        @foreach ($book->item->tags as $tag)
+                        @foreach ($musicAlbum->item->tags as $tag)
                             <span class="my-1 mx-1 px-1.5 bg-gray-400 text-white rounded-xl">
                                 {{ $tag->name }}
                             </span>
