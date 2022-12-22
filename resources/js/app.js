12673beef0188dfc3452226324ab4ce3ade032fd
mdram83 <103069429+mdram83@users.noreply.github.com>
2022-12-22 22:01:42 +0100

    issue with vite manifest in production: check if import of all library scripts in app.js will work.

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,9 +1,10 @@
 import './bootstrap';
 
-/*import.meta.glob([
-    './library/!*.js',
-    './library/ajax/!*.js',
-]);*/
+import.meta.glob([
+    '../js/library/ajax/isbn-details.js',
+    // './library/*.js',
+    // './library/ajax/*.js',
+]);
 
 import Alpine from 'alpinejs';
 
