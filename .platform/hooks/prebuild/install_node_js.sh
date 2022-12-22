faefec7e5e2a873896b870bfd471bba169b4edf4
mdram83 <103069429+mdram83@users.noreply.github.com>
2022-12-22 16:04:29 +0100

    build: adjusting node.js installation process

--- .platform/hooks/prebuild/install_node_js.sh
+++ .platform/hooks/prebuild/install_node_js.sh
@@ -2,14 +2,14 @@
 
 # Install  Node  alongside with the paired NPM release
 
-if [[ ! "$(node --version)" =~ "v12" ]]; then
+if [[ ! "$(node --version)" =~ "v16" ]]; then
     sudo yum remove -y nodejs npm
 
     sudo rm -fr /var/cache/yum/*
 
     sudo yum clean all
 
-    curl --silent --location https://rpm.nodesource.com/setup_12.x | sudo bash -
+    curl --silent --location https://rpm.nodesource.com/setup_16.x | sudo bash -
 
     sudo yum install nodejs -y
 fi
