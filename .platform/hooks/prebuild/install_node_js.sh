1298a93a04078df20d4992979072897cbbf41ee0
mdram83 <103069429+mdram83@users.noreply.github.com>
2022-12-22 15:18:18 +0100

    build: node v12

--- .platform/hooks/prebuild/install_node_js.sh
+++ .platform/hooks/prebuild/install_node_js.sh
@@ -2,14 +2,14 @@
 
 # Install  Node  alongside with the paired NPM release
 
-if [[ ! "$(node --version)" =~ "v18" ]]; then
+if [[ ! "$(node --version)" =~ "v12" ]]; then
     sudo yum remove -y nodejs npm
 
     sudo rm -fr /var/cache/yum/*
 
     sudo yum clean all
 
-    curl --silent --location https://rpm.nodesource.com/setup_18.x | sudo bash -
+    curl --silent --location https://rpm.nodesource.com/setup_12.x | sudo bash -
 
     sudo yum install nodejs -y
 fi
