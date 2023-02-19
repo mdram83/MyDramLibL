03f935cb9c9204b1ded77d2b6f2c880dd1dcab32
mdram83 <103069429+mdram83@users.noreply.github.com>
2023-02-19 16:25:57 +0100

    Initial changes in Dashboard controller (show item ids)

--- app/Http/Controllers/Dashboard.php
+++ app/Http/Controllers/Dashboard.php
@@ -8,6 +8,21 @@ class Dashboard extends Controller
 {
     public function __invoke()
     {
-        return view('dashboard');
+
+        // recently added items
+
+        // TODO need to figure out how to link user to proper module from itemable type
+        // TODO should I have some config array for that (with class name, router name, blade prefix etc.)?
+        $items = auth()->user()->items()->withOnly([])->latest()->take(5)->get();
+
+        // recently played music albums
+
+        // recently added friends' items
+
+        // recently added friends
+
+        return view('dashboard', [
+            'items' => $items,
+        ]);
     }
 }
